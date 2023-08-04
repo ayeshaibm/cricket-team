7e246ef33a45782e42e537a55cfbb9e2eee02833
ayeshaibm <angelayesha13@gmail.com>
2023-08-04 15:52:25 +0000

    adding to thread

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("node:path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+const intializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3004, () => {
+      console.log("Server running at http://localhost:3004/");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+  }
+};
+intializeDBAndServer();
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+app.get("/players/", async (request, response) => {
+  const getPlayersQuery = `
+    SELECT *
+    FROM
+    cricket_team
+    ORDER BY
+    player_id:`;
+  const playersArray = await db.all(getPlayersQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
